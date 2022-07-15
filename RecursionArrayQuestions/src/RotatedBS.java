a6e52d7ab5f49349194a6265433b7f42b1372bfe
Deepak <deepakgunpal0@gmail.com>
2022-07-16 01:51:23 +0530

    rotateArray

--- RecursionArrayQuestions/src/RotatedBS.java
+++ RecursionArrayQuestions/src/RotatedBS.java
@@ -1,10 +1,63 @@
+import java.util.Arrays;
+
 public class RotatedBS {
     public static void main(String[] args) {
-        int[] arr = {5,6,7,8,9,1,2,3};
-        int target = 7;
-        System.out.println(rotatedBS(arr,target,0,arr.length-1));
+//        int[] arr = {5,6,7,8,9,1,2,3};
+//        int target = 5;
+//        System.out.println(rotatedBS(arr,target,0,arr.length-1));
+        //leet code question
+        int[] arr = {1,2,3,4,5,6,7};
+        int k = 3;
+//        System.out.println(Arrays.toString(rotate(arr, k)));
+        System.out.println(Arrays.toString(rotateBetter(arr, k)));
     }
 
+    //leetcode - https://leetcode.com/problems/rotate-array/
+        static int[] rotate(int[] nums, int k) {
+            if(k>nums.length) k %= nums.length;
+            if (k==nums.length) return nums;
+            int[] arr = new int[nums.length];
+            if (k>nums.length/2) {
+                for (int i = 0; i < k; i++) {
+                    arr[i] = nums[i];
+                    nums[i] = nums[nums.length - k + i];
+                }
+
+                for (int i = k; i < nums.length; i++) {
+                    nums[i] = arr[i - k];
+                }
+            }else {
+                for (int i = nums.length-1; i >= k; i--) {
+                    arr[i] = nums[i];
+                    nums[i] = nums[i - k];
+                }
+
+                for (int i = 0; i < k; i++) {
+                    nums[i] = arr[arr.length - k + i];
+                }
+            }
+            return nums;
+        }
+
+        //better approach
+    static int[] rotateBetter(int[] nums, int k) {
+        if(k>nums.length) k %= nums.length;
+        if(k==nums.length) return nums;
+        int[] arr = new int[nums.length];
+        for(int i=k; i<nums.length; i++){
+            arr[i] = nums[i-k];
+        }
+        for(int i=0; i<k; i++){
+            arr[i] = nums[nums.length-k+i];
+        }
+
+        for(int i=0; i<nums.length; i++){
+            nums[i] = arr[i];
+        }
+        return nums;
+    }
+
+        //rotated binary search
     private static int rotatedBS(int[] arr, int target, int s,int e) {
         if (s>e) return -1;
         int m = s + (e -s)/2;
