38495be37a152eae29b7b7ccc0ddc72452005da1
Deepak <deepakgunpal0@gmail.com>
2022-07-15 11:27:21 +0530

    maths

---
+++ Maths/GCD_LCM.java
@@ -0,0 +1,20 @@
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println(gcd(4,9));
+        System.out.println(lcm(2,7));
+    }
+
+//    lowest number divisible by both number
+    //lowest common multiple
+    private static int lcm(int a, int b) {
+        return a * b/ gcd(a,b);
+    }
+
+    // highest common factor
+    private static int gcd(int a, int b) {
+        if (a==0) return b;
+        return gcd(b%a,a);
+    }
+
+
+}
